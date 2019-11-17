8b191d2f1a6b89a49b73728d6a2fcc2b4148301e
ddreise <ddreise@gmail.com>
2019-11-16 21:37:09 -0500

    Server initialized

--- src/main.c
+++ src/main.c
@@ -1,7 +1,69 @@
+// main.c
+//
+// Operating Systems - Assignment 4
+//
+// Purpose: Server/client relationship where server makes 1024 bytes of shared memory
+//			and fills with 32 random letters between A and T every 0.25 seconds.
+//			The client retreives data from shared memory and creates a histogram that
+//			indicates the frequency of each letter.
+//
+// 
+// Daniel Dreise - All Rights Reserved
+// November 16, 2019
+
+
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024		// Size of shared memory
 
 int main(int argc, char *argv[]) {
-	printf("Hello World");
+
+	key_t key;			// Key variable
+	int shmid;			// Shared memory ID
+	char* data;			// shared memory pointer
+	
+	// Make the "key" identifier to give to each process so they can access shared memory
+	if ((key = ftok(".", 'A')) == -1) {
+		perror("ftok failed\n");
+		exit(1);
+	}
+
+	// Connect to shared memory. If not created, create one (IPC_CREAT)
+	if ((shmid = shmget(key, SHM_SIZE, 0640 | IPC_CREAT)) == -1){		// 0640 - creator read/write
+		perror("shmget failed\n");
+		exit(2);
+	}
+
+	// Attach to the recently created segment
+	// shmat attaches to shared memory segment identified by shmid to the address space of the calling process
+	data = shmat(shmid, (void *)0, 0);			
+	if (data == (char *) (-1)){
+		perror("shmat failed\n");
+		exit (3);
+	}
+
+	/*********** MAIN FUNCTION OF SERVER *************/
+
+	// Generate random letters and print to shared memory
+	
+
+
+	/*********** END OF SERVER FUNCTION **************/
+
+	// Detach from segment if finished
+	if (shmdt(data) == -1){
+		perror("shmdt failed\n");
+		exit(4);
+	}
+
+
 
 	return(0);
 }
